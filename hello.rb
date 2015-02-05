14b39e138e952b5bc6893e7ce62b0a6be81abe78
Ben Swift <benswift404@users.noreply.github.com>
2015-02-05 07:53:33 -0500

    Update hello.rb

--- hello.rb
+++ hello.rb
@@ -1,2 +1,18 @@
 class BankAccount
+  def initialize(firstname, lastname)
+    @balance = 0
+    @firstname = firstname
+    @lastname = lastname
+  end
+  
+  def deposit(amount)
+    @balance += amount
+  end
+  
+  def withdraw(amount)
+    @balance -= amount
+  end
+end
+
+# bankaccount = BankAccount.new("Ben", "Swift")
   
