91737f198a54ccb49cfcd013e453e11be47f9540
Ben Swift <benswift404@users.noreply.github.com>
2015-02-05 08:54:34 -0500

    Update greeter.rb

--- greeter.rb
+++ greeter.rb
@@ -1,1 +1,15 @@
-class
+class Greeter
+  attr_accessor :name
+  def initialize(name = "World")
+    @name = name
+  end
+  
+  def hi
+    puts "Hello, #{@name}!"
+  end
+  
+  def bye 
+    puts "Bye #{@name}, Come back soon!"
+  end
+end
+
